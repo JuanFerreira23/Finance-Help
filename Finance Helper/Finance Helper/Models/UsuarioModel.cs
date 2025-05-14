db46487949bd92047528a6ed8203a533ab640457
JuanFerreira23 <juan.ralf@outlook.com>
2025-05-14 12:32:09 -0300

    Update UsuarioModel.cs

--- Finance Helper/Finance Helper/Models/UsuarioModel.cs
+++ Finance Helper/Finance Helper/Models/UsuarioModel.cs
@@ -6,11 +6,11 @@ namespace Finance_Helper.Models
     {
         [Key]
         public int Id { get; set; }
-
+        [Required]
         public string Nome { get; set; }
-
+        [Required, EmailAddress]
         public string Email { get; set; }
-
+        [Required]
         public string Senha { get; set; }
 
         public string Telefone { get; set; }
